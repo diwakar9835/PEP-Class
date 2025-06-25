25f8e7c0114793ef3ceedf24458a11dfd4c8a0be
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2025-06-25 09:22:44 +0530

    Add files via upload

---
+++ 20Proj/mov.js
@@ -0,0 +1,56 @@
+
+{
+
+  "page": 1,
+  "results": [
+    {
+      "adult": false,
+      "backdrop_path": "/uIpJPDNFoeX0TVml9smPrs9KUVx.jpg",
+      "genre_ids": [27, 9648],
+      "id": 574475,
+      "original_language": "en",
+      "original_title": "Final Destination Bloodlines",
+      "overview": "Plagued by a violent recurring nightmare, college student Stefanie heads home to track down the one person who might be able to break the cycle and save her family from the grisly demise that inevitably awaits them all.",
+      "popularity": 1205.5109,
+      "poster_path": "/6WxhEvFsauuACfv8HyoVX6mZKFj.jpg",
+      "release_date": "2025-05-14",
+      "title": "Final Destination Bloodlines",
+      "video": false,
+      "vote_average": 7.2,
+      "vote_count": 984
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/6van4BavoNXaZhCPdzLHNQ4Uc8H.jpg",
+      "genre_ids": [53, 28],
+      "id": 1087891,
+      "original_language": "en",
+      "original_title": "The Amateur",
+      "overview": "After his life is turned upside down when his wife is killed in a London terrorist attack, a brilliant but introverted CIA decoder takes matters into his own hands when his supervisors refuse to take action.",
+      "popularity": 377.9503,
+      "poster_path": "/SNEoUInCa5fAgwuEBMIMBGvkkh.jpg",
+      "release_date": "2025-04-09",
+      "title": "The Amateur",
+      "video": false,
+      "vote_average": 6.9,
+      "vote_count": 544
+    },
+    ...
+    {
+      "adult": false,
+      "backdrop_path": "/fPWJn5pqBr8n4h0YxW3QuasdvoI.jpg",
+      "genre_ids": [28, 53],
+      "id": 1127110,
+      "original_language": "en",
+      "original_title": "Diablo",
+      "overview": "Ex-con Kris Chaney seizes the daughter of a Colombian gangster to fulfill a noble promise to the young girl's mother. When her father enlists both the criminal underworld and a psychotic killer to exact his revenge, Kris relies on everything he's ever learned to stay alive and keep his word.",
+      "popularity": 236.1268,
+      "poster_path": "/uFQduVyYIinJy3eLjozgfl6Xtcn.jpg",
+      "release_date": "2025-06-13",
+      "title": "Diablo",
+      "video": false,
+      "vote_average": 7.312,
+      "vote_count": 201
+    }
+  ]
+}
